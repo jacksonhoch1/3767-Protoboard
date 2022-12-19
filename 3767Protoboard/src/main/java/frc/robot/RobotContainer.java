6cc23a9c11b818ce82f990a2f94c1ebee7aea889
Jacksonhoch1 <jacksonhoch1@gmail.com>
2022-12-19 13:52:33 -0500

    removed unnecessary libraries

--- 3767Protoboard/src/main/java/frc/robot/RobotContainer.java
+++ 3767Protoboard/src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,7 @@
 package frc.robot;
 
 //wpilib
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
