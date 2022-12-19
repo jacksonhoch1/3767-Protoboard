effa53d133a253b848195dcef510bafca73a6b2c
Jacksonhoch1 <jacksonhoch1@gmail.com>
2022-12-19 12:30:17 -0500

    started set test motors command

---
+++ 3767Protoboard/src/main/java/frc/robot/commands/SetTestingMotors.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Protoboard;
+
+public class SetTestingMotors extends CommandBase {
+  Protoboard protoboard;
+  Supplier<Double> falcon, neo;
+  /** Creates a new SetTestingMotors. */
+  public SetTestingMotors(Protoboard protoboard, Supplier<Double> falcon, Supplier<Double> neo) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.protoboard = protoboard;
+    this.falcon = falcon;
+    this.neo = neo;
+    addRequirements(protoboard);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    protoboard.setFalcon(falcon.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
