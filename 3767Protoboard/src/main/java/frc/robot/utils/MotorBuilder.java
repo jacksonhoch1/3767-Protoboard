6e91e13513f18b1d9695203bcc6126877dd901c2
Jacksonhoch1 <jacksonhoch1@gmail.com>
2023-01-10 14:50:14 -0500

    added NEO builder

--- 3767Protoboard/src/main/java/frc/robot/utils/MotorBuilder.java
+++ 3767Protoboard/src/main/java/frc/robot/utils/MotorBuilder.java
@@ -1,11 +1,16 @@
 package frc.robot.utils;
 
+//falcon
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+//neo
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class MotorBuilder {
     public MotorBuilder() {}
@@ -13,12 +18,13 @@ public class MotorBuilder {
     /**
      * Creates a new TalonFX motor controller (Falcon500). This was created to aid with creation of motors across multiple subsystems.
      * @param id The CAN Id of the motor controller.
-     * @param follow The motor to follow. Supports any CTRE motor controller. If none, specify null.
+     * @param leader The motor to follow. Supports any CTRE motor controller. If none, specify null.
      * @param neutralMode coast or brake when unpowered.
      * @param inverted Invert or not. Default is clockwise.
+     * If this motor is a follower default is followMaster, but if true, opposeMaster. 
      * @return The fully configured motor
      */
-    public WPI_TalonFX createFalcon(int id, BaseMotorController follow, NeutralMode neutralMode, boolean inverted) {
+    public WPI_TalonFX createFalcon(int id, BaseMotorController leader, NeutralMode neutralMode, boolean isInverted) {
         TalonFXConfiguration configs = new TalonFXConfiguration();
         configs.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
         WPI_TalonFX motor = new WPI_TalonFX(id);
@@ -27,16 +33,28 @@ public class MotorBuilder {
         motor.setNeutralMode(neutralMode);
         motor.configVoltageCompSaturation(12);
         motor.enableVoltageCompensation(true);
-        if (follow != null) {
-            motor.follow(follow);
-            motor.setInverted(TalonFXInvertType.FollowMaster);
-        } else if (inverted) {
-            motor.setInverted(TalonFXInvertType.CounterClockwise);
+        if (leader != null) {
+            motor.follow(leader);
+            if (isInverted) {
+                motor.setInverted(TalonFXInvertType.OpposeMaster);
+            } else {
+                motor.setInverted(TalonFXInvertType.FollowMaster);;
+            }
         } else {
-            motor.setInverted(TalonFXInvertType.Clockwise);
+            motor.setInverted(isInverted);
         }
         return motor;
     }
 
-    //TODO: NEO support
+    public CANSparkMax createNeo(int id, CANSparkMax leader, IdleMode idleMode, boolean isInverted) {
+        CANSparkMax motor = new CANSparkMax(id, MotorType.kBrushless);
+        motor.restoreFactoryDefaults();
+        motor.setIdleMode(idleMode);
+        if (leader != null) {
+            motor.follow(leader);
+        } else if (isInverted) {
+            motor.setInverted(isInverted);
+        }
+        return motor; 
+    }
 }
